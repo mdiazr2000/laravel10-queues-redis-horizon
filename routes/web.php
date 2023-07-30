a423d7426a0f021a2c3015a81629c6f1697f6801
Marianela Diaz <mdiazr2000@gmail.com>
2023-07-29 22:01:18 -0300

    Horizon and redis

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Redis;
+
+Route::get('test-job-redis', function() {
+    \App\Jobs\TestJob::dispatch(['mail' => 'mdiazr2000@gmail.com']);
+});
+
+Route::get('test-cache-redis', function() {
+    \Cache::store('redis')->put('Laradock', 'Awesome', 100);
+});
